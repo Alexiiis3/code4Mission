ec31394bfd1e0572ff4fe0fe4c463a6b3440df39
Alexis <100202060+Alexiiis3@users.noreply.github.com>
2022-02-24 08:51:38 +0100

    mixed pression reading code
    mixed pression reading code with tempertaure code

--- tempPressureReadAndEmit/tempPressureReadAndEmit.ino
+++ tempPressureReadAndEmit/tempPressureReadAndEmit.ino
@@ -13,19 +13,33 @@
  *
  * CANSAT SINESTUDIOS TEAM - IES Villaverde
  */
+#define kpa2atm 0.00986923267
+
 
 int R1=10000; // the resistor from voltage divider
 float Bt=3977.0;// B parameter
 float Tc=0;// temperature in celsius
+
+// pin defs
+int pressurePin = 0;
+
+// variables
+int val;
+float pkPa; // pressure in kPa
+float pAtm; // pressure in Atm
+
+
+
 void setup(void) {
  Serial.begin(9600);
 }
 void loop(void) { 
   updatetemp();
+  updatePression();
 }   
 void updatetemp()// function which generates temperature
 {
- int x=analogRead(A1);//reading the value from Analog pin 1
+  int x=analogRead(A1);//reading the value from Analog pin 1
   float Vout=x*(5/1023.0);// transform it in voltage
   float Rt=R1*Vout/(5.0-Vout);// the resistance of thermistor from the voltage divider
                               //this value cance with temperature
@@ -37,4 +51,25 @@ void updatetemp()// function which generates temperature
   float  T=1/y1;// temperature in kelvin
         Tc=T-273.15;//temperature in celsius
         Serial.println(Tc);// display temperature in serial monitor
+}
+void updatePression() //To read pression from mpx
+{
+   /* get and print current time */
+  time = millis()/1000;
+  Serial.print(time);
+  Serial.print(": ");
+
+  /* get the pressure */
+  val = analogRead(pressurePin);
+  pkPa = ((float)val/(float)1023+0.095)/0.009;
+  pAtm = kpa2atm*pkPa;
+   
+  /* send pressure to serial port */
+  Serial.print(pkPa);
+  Serial.print("kPa ");
+  Serial.print(pAtm);
+  Serial.print("Atm \n");
+  
+   
+  delay(1000);
 }
